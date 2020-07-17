69e8c5302b498d5f839d9c74f2ec9a0e83229f11
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-17 12:48:31 -0600

    step 5 done

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -20,15 +20,25 @@ const age = 26
 
 // Code Here
 
+const name  = `Kody`
+
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
 // Code Here
 
+const phrase = ``
+
 //////////////////STEP 5////////////////////
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
+
+if ( lovesCode === true){
+    return `I love to code`
+} else {
+    `I do not love to code`
+}
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
